5f59141225bb88ee4d0eec4b7d0246bb1efb688e
andersonsimplicio <andeson25eu@gmail.com>
2022-06-01 20:05:19 -0300

    Configuração Tailwind

---
+++ frontend/src/apps/components/home/Navbar/index.tsx
@@ -0,0 +1,9 @@
+
+
+
+export function Navbar() {
+    return ( 
+        <div  className="bg-slate-500">
+            Navbar 
+        </div> );
+}
---
+++ frontend/src/apps/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../../components/home/Navbar";
+
+
+export function Home() {
+    return ( 
+    <>
+        <Navbar />
+         
+    </> );
+}
+
